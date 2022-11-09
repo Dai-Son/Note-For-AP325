4ee90b6c29815bbed6b60bd2397d58909981562d
DaiSon <61372575+Dai-Son@users.noreply.github.com>
2022-11-09 21:14:48 +0800

    Fix markdown errors, Done Update P-2-3

---
+++ 第二章/P-2-3.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define StarBurstStream ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define int long long
+using namespace std;
+
+int recur_exp(int x, int y, int p);
+
+signed main(void){
+
+	StarBurstStream
+	long long x, y, p;
+
+	cin >> x >> y >> p;
+	cout << recur_exp(x, y, p) << '\n';
+
+	return 0;
+
+}
+
+int recur_exp(int x, int y, int p)
+{
+
+	int t;
+
+	if(p == 1) return 0;
+	if(y == 0) return 1;
+
+	//y¬O©_¼Æ
+	if(y & 1){
+		return (recur_exp(x,y-1,p)*x) % p;
+	}
+
+	t = recur_exp(x ,y/2 ,p);
+	return (t*t)%p;
+}
