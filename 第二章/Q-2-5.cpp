b713c16d5dd5e7463dabc13341655978a0a13384
DaiSon <61372575+Dai-Son@users.noreply.github.com>
2022-11-11 01:37:25 +0800

    Update AC code of Q-2-5

--- 第二章/Q-2-5.cpp
+++ 第二章/Q-2-5.cpp
@@ -1,24 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define StarBurstStream ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define int unsigned long long
 //快速冪
-int recur_exp(int n);
+vector<vector<int>> recur_exp(int n);
 
 //矩陣乘法
-int multipy(int a[][2], int b[][2]);
+vector<vector<int>> multipy(vector<vector<int>> a, vector<vector<int>> b);
 
-//總結
 int fib(int m);
 
 //矩陣
-int tmp[2][2];
-int BASE[2][2]={
-                { 1 , 1 },
-                { 1 , 0 }
-            };
+vector<vector<int>> tmp( 2, vector<int> ( 2 , 0 ) );
+vector<vector<int>> A
+{
+    { 1 , 1 },
+    { 1 , 0 }
+};
 
+int P = 1000000007;
 signed main(){
 
+    StarBurstStream
     int in;
 
     while( cin >> in && in != -1 ){
@@ -27,34 +30,35 @@ signed main(){
     return 0;
 }
 
-int recur_exp(int n){
+vector<vector<int>> recur_exp(int n){
 
-    if( n == 1 ) return BASE;
+    if( n == 1 ) return A;
 
     //n是奇數
     else if( n & 1 ){
         tmp = recur_exp((n-1)/2);
-        return multipy(BASE, multipy(BASE, tmp));
+        return multipy(multipy(tmp, tmp), A);
     }
 
     tmp = recur_exp(n/2);
-    return multipy(BASE,  tmp);
+    return multipy(tmp, tmp);
 
 }
 
-int multipy(int a[][2], int b[][2]){
-    int c1 = a[0][0]*b[0][0] + a[0][1]*b[1][0];
-    int c2 = a[0][0]*b[0][1] + a[0][1]*b[1][1];
-    int c3 = a[1][0]*b[0][0] + a[1][1]*b[1][0];
-    int c4 = a[1][0]*b[0][1] + a[1][1]*b[1][1];
-    int c[2][2]={
-                    { c1 , c2 },
-                    { c3 , c4 }
-                };
+vector<vector<int>> multipy(vector<vector<int>> a, vector<vector<int>> b){
+    int c1 = (a[0][0] * b[0][0] + a[0][1] * b[1][0]) % P;
+    int c2 = (a[0][0] * b[0][1] + a[0][1] * b[1][1]) % P;
+    int c3 = (a[1][0] * b[0][0] + a[1][1] * b[1][0]) % P;
+    int c4 = (a[1][0] * b[0][1] + a[1][1] * b[1][1]) % P;
+    vector<vector<int>> c
+    {
+        { c1 , c2 },
+        { c3 , c4 }
+    };
     return c;
 }
 
 int fib(int m){
     if( m <= 1 ) return m;
-    return recur_exp(m);
+    return recur_exp(m-1)[0][0];
 }
--- 第二章/README.md
+++ 第二章/README.md
@@ -521,3 +521,72 @@ $$
 ## Q-2-5
 ### 題目
 [![Q-2-5](https://cdn.discordapp.com/attachments/988162819679715408/1039923726315167814/image.png "Q-2-5")](https://judge.tcirc.tw/ShowProblem?problemid=d014 "Q-2-5")
+### 作法
+##### 我的作法
+```cpp
+#include <bits/stdc++.h>
+using namespace std;
+#define StarBurstStream ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define int unsigned long long
+//快速冪
+vector<vector<int>> recur_exp(int n);
+
+//矩陣乘法
+vector<vector<int>> multipy(vector<vector<int>> a, vector<vector<int>> b);
+
+int fib(int m);
+
+//矩陣
+vector<vector<int>> tmp( 2, vector<int> ( 2 , 0 ) );
+vector<vector<int>> A
+{
+    { 1 , 1 },
+    { 1 , 0 }
+};
+
+int P = 1000000007;
+signed main(){
+
+    StarBurstStream
+    int in;
+
+    while( cin >> in && in != -1 ){
+        cout << fib(in) << '\n';
+    }
+    return 0;
+}
+
+vector<vector<int>> recur_exp(int n){
+
+    if( n == 1 ) return A;
+
+    //n是奇數
+    else if( n & 1 ){
+        tmp = recur_exp((n-1)/2);
+        return multipy(multipy(tmp, tmp), A);
+    }
+
+    tmp = recur_exp(n/2);
+    return multipy(tmp, tmp);
+
+}
+
+vector<vector<int>> multipy(vector<vector<int>> a, vector<vector<int>> b){
+    int c1 = (a[0][0] * b[0][0] + a[0][1] * b[1][0]) % P;
+    int c2 = (a[0][0] * b[0][1] + a[0][1] * b[1][1]) % P;
+    int c3 = (a[1][0] * b[0][0] + a[1][1] * b[1][0]) % P;
+    int c4 = (a[1][0] * b[0][1] + a[1][1] * b[1][1]) % P;
+    vector<vector<int>> c
+    {
+        { c1 , c2 },
+        { c3 , c4 }
+    };
+    return c;
+}
+
+int fib(int m){
+    if( m <= 1 ) return m;
+    return recur_exp(m-1)[0][0];
+}
+
+```
