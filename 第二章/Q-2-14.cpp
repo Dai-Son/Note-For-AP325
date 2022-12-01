cdb927580dc6e4886344eb6f4d7712895c88194d
DaiSon <61372575+Dai-Son@users.noreply.github.com>
2022-12-02 00:57:13 +0800

    Update Q-2-14.cpp

--- 第二章/Q-2-14.cpp
+++ 第二章/Q-2-14.cpp
@@ -2,28 +2,52 @@
 using namespace std;
 #define StarBurstStream ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define int long long
-void rec(vector<int> wall, int left, int right, int width);
+
+int ans[100005];
+
+map<int,int> h;
+map<int,int> rh;
+
+int hl[100005],hr[100005];
+
+void rec( int l, int r, int w );
 
 signed main(){
 
     StarBurstStream
-    int fi;
+    int fi, fo;
 
     int n, i, w;cin >> n >> i >> w;
 
-    vector<int> wall;
-    int highest=-1;
-    wall.push_back(100000001);
-    for( fi = 0 ; fi < n ; fi++ ){
-        int in;cin >> in;
-        wall.push_back(in);
-        highest = max(in, highest);
+    h.clear();
+    rh.clear();
+
+    for( fi = 1 ; fi <= n ; fi++ ){
+        int tmp;
+        cin >> tmp;
+        h[fi] = tmp;
+        rh[tmp] = fi;
     }
-    wall.push_back(100000001);
-    int hloc = lower_bound(wall.begin(),wall.end(),highest)-wall.begin();
+    h[0] = 1000000005;
+    h[n+1] = 1000000006;
+
+    for( fi = 1 ; fi <= n ; fi++ ){
+        for( fo = fi-1 ; fo >= 0 ; fo-- ){
+            if( h[fo] > h[fi] ){
+                hl[fi] = fo;
+            }
+        }for( fo = fi+1 ; fo <= n+1 ; fo++ ){
+            if( h[fo] > h[fi] ){
+                hr[fi] = fo;
+            }
+        }
+    }
+
+
+
 
 
 }
-void rec(int amount, int left, int right){
+void rec( int l, int r, int w ){
 
 }
