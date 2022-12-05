5dcb4fa43e25519234a3565969f2be82e56d2c86
DaiSon <61372575+Dai-Son@users.noreply.github.com>
2022-12-06 00:46:47 +0800

    Create P-2-15.cpp

---
+++ 第二章/P-2-15.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define StarBurstStream ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#int long long
+
+signed main(){
+
+    StarBurstStream
+
+    int n, m;cin >> n >> m;
+
+
+}
