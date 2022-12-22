b20dc72ed7134444621fef1df67eb1d83fa82953
DaiSon <61372575+Dai-Son@users.noreply.github.com>
2022-12-23 01:13:03 +0800

    Update Q-3-3.cpp

--- 第三章/Q-3-3.cpp
+++ 第三章/Q-3-3.cpp
@@ -4,25 +4,11 @@ using namespace std;
 #define StarBurstStream ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define int long long
 
-stack<int> Snum;
-stack<int> Scalc;
+void intopo(char in, char po);
 
-void
 signed main(){
-    StarBurstStream
 
     char in[110];
-    char tran[16]="0123456789+-*/";
-    cin >> in;
-
-    Snum(strchr(tran, in[0]) - tran);
-
-    int len = strlen(in);
-
-    for( int i = 1; i < len ; i = i + 2 ){
-        Scalc.push(strchr(tran, in[i]) - tran);
-        Snum.push(strchr(tran, in[i+1]) - tran);
-    }
 
 
 }
