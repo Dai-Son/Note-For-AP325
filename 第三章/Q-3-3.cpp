5edbe2d76ce2ed3c249bfb99654add85b42dc211
DaiSon <61372575+Dai-Son@users.noreply.github.com>
2022-12-27 23:50:27 +0800

    Update Q-3-3.cpp

--- 第三章/Q-3-3.cpp
+++ 第三章/Q-3-3.cpp
@@ -4,11 +4,25 @@ using namespace std;
 #define StarBurstStream ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define int long long
 
-void intopo(char in, char po);
+int pr(string in);
 
 signed main(){
 
+    int i;
+
     char in[110];
+    cin >> in;
+    len = strlen(in);
 
+    Stack<string> S;
 
+    for( i = 0 ; i < len ; i++ )
+
+}
+int pr(char in){
+    switch(in){
+        case '+': case '-': return 1;
+        case '*': case '/': return 2;
+        default: return 0;
+    }
 }
