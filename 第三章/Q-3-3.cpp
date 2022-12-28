2784a93bb4e21e4273367ae8b6372ad6f04e2447
DaiSon <61372575+Dai-Son@users.noreply.github.com>
2022-12-29 00:06:57 +0800

    Update Q-3-3.cpp (AC code)

--- 第三章/Q-3-3.cpp
+++ 第三章/Q-3-3.cpp
@@ -5,14 +5,13 @@ using namespace std;
 #define int long long
 
 int pr(char in);
-
+int eval(char* post, int len);
 signed main(){
 
-    char in[110];
-    char postfix[110];
+    char in[110] = {'\0'};
+    char postfix[110] = {'\0'};
     cin >> in;
     int len = strlen(in);
-    cout << len;
     stack<char> S;
     S.push('|');
     int i, j;
@@ -27,7 +26,12 @@ signed main(){
             postfix[j++] = in[i];
         }
     }
-    cout << postfix;
+    while( S.top() != '|' ){
+        postfix[j++] = S.top();
+        S.pop();
+    }
+
+    cout << eval(postfix , len) << '\n';
 
 
 }
@@ -37,4 +41,35 @@ int pr(char in){
         case '*': case '/': return 2;
         default: return 0;
     }
+}
+int eval(char* post, int len){
+    stack<int> S;
+    int i;
+    for( i = 0 ; i < len ; i++){
+        if( post[i]-'0' < 0 ){
+            int num2 = S.top();
+            S.pop();
+            int num1 = S.top();
+            S.pop();
+            switch(post[i]){
+                case '+':
+                    S.push(num1+num2);
+                    break;
+                case '-':
+                    S.push(num1-num2);
+                    break;
+                case '*':
+                    S.push(num1*num2);
+                    break;
+                case '/':
+                    S.push(num1/num2);
+                    break;
+                default:
+                    break;
+            }
+        }else{
+            S.push(post[i]-'0');
+        }
+    }
+    return S.top();
 }
