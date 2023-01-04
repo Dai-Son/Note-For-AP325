ce26a0b7cb58afa34ba632dd46ab3a39b37ab90e
DaiSon <61372575+Dai-Son@users.noreply.github.com>
2023-01-05 00:21:31 +0800

    Update P-3-4.cpp

--- 第三章/P-3-4.cpp
+++ 第三章/P-3-4.cpp
@@ -1,7 +1,31 @@
 #include <bits/stdc++.h>
+#include <limits>
 using namespace std;
 
 #define StarBurstStream ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define int long long
+#define N 200010
+
+int line[N];
+stack<int> S;
 signed main(){
+    int MAXVAL = numeric_limits<int>::max();
+    int i;
+
+    int ans = 0;
+    S.push(0);
+    line[0] = MAXVAL;
+
+    int n;cin >> n;
+    for( i = 1 ; i <= n ; i++ ){
+        cin >> line[i];
+    }
+
+    for( i = 1 ; i <= n ; i++ ){
+        while(line[S.top()] <= line[i])
+            S.pop();
+        ans+= i - S.top();
+        S.push(i);
+    }
+    cout << ans << '\n';
 }
